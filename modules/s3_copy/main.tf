0a87f856a21917a51015319fdcc2bff2ce4818db
Ajay sharma <ajay.sharma1984@gmail.com>
2022-07-11 00:15:45 -0400

    adding support for setting mime type based on file extension

--- modules/s3_copy/main.tf
+++ modules/s3_copy/main.tf
@@ -1,13 +1,15 @@
 locals {
   clean_key_prefix = trimsuffix(var.key_prefix, "/")
+  mime_types       = jsondecode(file("${path.module}/mime.json"))
 }
 data "aws_s3_bucket" "selected" {
   bucket = var.deploy_bucket
 }
 resource "aws_s3_object" "object" {
-  for_each = fileset(var.deploy_source, "**")
-  bucket   = data.aws_s3_bucket.selected.id
-  key      = "${local.clean_key_prefix}${each.value}"
-  source   = "${var.deploy_source}/${each.value}"
-  etag     = filemd5("${var.deploy_source}/${each.value}")
+  for_each     = fileset(var.deploy_source, "**")
+  bucket       = data.aws_s3_bucket.selected.id
+  key          = "${local.clean_key_prefix}${each.value}"
+  source       = "${var.deploy_source}/${each.value}"
+  etag         = filemd5("${var.deploy_source}/${each.value}")
+  content_type = lookup(local.mime_types, regex("\\.[^.]+$", each.value), null)
 }
